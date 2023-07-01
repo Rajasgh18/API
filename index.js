cda36e3d04912467a51a93ff9b59c7ef30d754b9
Raja Singh <rajasgh18@github.com>
2023-07-01 23:11:45 +0530

    changed

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const App = express();
 const port = 5000;
-const dotenv = require('dotenv');
-const connectToDB = require('./db');
-const product = require('./routes/product');
-const category = require('./routes/category');
-const image = require('./routes/image');
-const user = require('./routes/user');
-const order = require('./routes/order');
+// const dotenv = require('dotenv');
+// const connectToDB = require('./db');
+// const product = require('./routes/product');
+// const category = require('./routes/category');
+// const image = require('./routes/image');
+// const user = require('./routes/user');
+// const order = require('./routes/order');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
 
-dotenv.config();
-connectToDB();
+// dotenv.config();
+// connectToDB();
 App.use(cors(
     {
         origin: 'https://king-kart.vercel.app/',
@@ -30,11 +30,11 @@ App.use('/', (req, res)=> {
     res.send("Hello");
 })
 
-App.use('/api/product', product);
-App.use('/api/category', category);
-App.use('/api/image', image);
-App.use('/api/user', user);
-App.use('/api/order', order);
+// App.use('/api/product', product);
+// App.use('/api/category', category);
+// App.use('/api/image', image);
+// App.use('/api/user', user);
+// App.use('/api/order', order);
 express.static(path.join(__dirname, "./public/assets")),
 
 App.listen(port, () => {
